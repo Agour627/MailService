e38b0b57f30560c76a3c86d1a91baed48771ad4a
Agour <mohamedagour627@gmail.com>
2022-01-16 11:58:12 +0200

    Upload MailService Project

---
+++ SendEmailsWithDotNet5/Controllers/MailingController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SendEmailsWithDotNet5.Dtos;
+using SendEmailsWithDotNet5.Services;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SendEmailsWithDotNet5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MailingController : ControllerBase
+    {
+        private readonly IMailingService _mailingService;
+
+        public MailingController(IMailingService mailingService)
+        {
+            _mailingService = mailingService;
+        }
+
+        [HttpPost("send")]
+        public async Task<IActionResult> SendMail([FromForm] MailRequestDto dto)
+        {
+            await _mailingService.SendEmailAsync(dto.ToEmail, dto.Subject, dto.Body, dto.Attachments);
+            return Ok();
+        }
+      
+    }
+}
